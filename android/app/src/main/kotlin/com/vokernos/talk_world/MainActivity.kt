f900ccc178f2e867acfeaa5e7e1b4490e41a65d8
abderazak-py <achourabderazak1@gmail.com>
2022-10-19 22:11:39 +0100

    Removing ignored files

--- android/app/src/main/kotlin/com/vokernos/talk_world/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.vokernos.talk_world
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/package_name/talk_world/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package package_name.talk_world
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
