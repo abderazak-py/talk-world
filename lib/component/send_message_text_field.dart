66ade27611cdc19c590565cf7c7829d7fe6abc63
abderazak-py <achourabderazak1@gmail.com>
2022-10-27 09:22:10 -0700

    dont allow messages without internet connection

--- lib/component/send_message_text_field.dart
+++ lib/component/send_message_text_field.dart
@@ -5,6 +5,9 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:top_snackbar_flutter/custom_snack_bar.dart';
+import 'package:top_snackbar_flutter/top_snack_bar.dart';
 import 'consts.dart';
 
 class SendMessageTextField extends StatelessWidget {
@@ -14,29 +17,42 @@ class SendMessageTextField extends StatelessWidget {
   final String collection;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, [bool mounted = true]) {
     final ImagePicker picker = ImagePicker();
     final messageController = TextEditingController();
     final scroolController = ScrollController();
 
     return TextField(
       controller: messageController,
-      onSubmitted: (data) {
+      onSubmitted: (data) async {
         if (messageController.text.trim() != '' &&
             messageController.text.trim() != '') {
           final user = FirebaseAuth.instance.currentUser;
-          messages.add(
-            {
-              'message': messageController.text,
-              'date': DateTime.now(),
-              'id': user!.email,
-              'name': user.displayName,
-              'uid': user.uid,
-              'type': 'text',
-              'profilePhoto': user.photoURL,
-            },
-          );
-          messageController.clear();
+          bool result = await InternetConnectionChecker().hasConnection;
+          if (result == true) {
+            messages.add(
+              {
+                'message': messageController.text,
+                'date': DateTime.now(),
+                'id': user!.email,
+                'name': user.displayName,
+                'uid': user.uid,
+                'type': 'text',
+                'profilePhoto': user.photoURL,
+              },
+            );
+            messageController.clear();
+          } else {
+            if (!mounted) return;
+            showTopSnackBar(
+              context,
+              CustomSnackBar.error(
+                message:
+                    "Oh! There's no internet connection. Please check your internet and try again",
+              ),
+            );
+          }
+
           scroolController.animateTo(0,
               duration: Duration(milliseconds: 500), curve: Curves.easeIn);
         }
@@ -78,29 +94,40 @@ class SendMessageTextField extends StatelessWidget {
           children: [
             IconButton(
               onPressed: () async {
-                XFile? image =
-                    await picker.pickImage(source: ImageSource.gallery);
-                if (image != null) {
-                  final file = File(image.path);
-                  final user = FirebaseAuth.instance.currentUser;
-                  final path = 'files/images/${user!.email}/${image.name}';
-                  final ref = FirebaseStorage.instance.ref().child(path);
-                  UploadTask? uploadTask;
-                  uploadTask = ref.putFile(file);
-                  final snapshot = await uploadTask.whenComplete(() {});
-                  final urlDownload = await snapshot.ref.getDownloadURL();
-                  messages.add(
-                    {
-                      'message': urlDownload,
-                      'date': DateTime.now(),
-                      'id': user.email,
-                      'name': user.displayName,
-                      'uid': user.uid,
-                      'type': 'image',
-                      'profilePhoto': user.photoURL,
-                    },
+                bool result = await InternetConnectionChecker().hasConnection;
+                if (result == true) {
+                  XFile? image =
+                      await picker.pickImage(source: ImageSource.gallery);
+                  if (image != null) {
+                    final file = File(image.path);
+                    final user = FirebaseAuth.instance.currentUser;
+                    final path = 'files/images/${user!.email}/${image.name}';
+                    final ref = FirebaseStorage.instance.ref().child(path);
+                    UploadTask? uploadTask;
+                    uploadTask = ref.putFile(file);
+                    final snapshot = await uploadTask.whenComplete(() {});
+                    final urlDownload = await snapshot.ref.getDownloadURL();
+                    messages.add(
+                      {
+                        'message': urlDownload,
+                        'date': DateTime.now(),
+                        'id': user.email,
+                        'name': user.displayName,
+                        'uid': user.uid,
+                        'type': 'image',
+                        'profilePhoto': user.photoURL,
+                      },
+                    );
+                  }
+                } else {
+                  if (!mounted) return;
+                  showTopSnackBar(
+                    context,
+                    CustomSnackBar.error(
+                      message:
+                          "Oh! There's no internet connection. Please check your internet and try again",
+                    ),
                   );
-                  //image = null;
                 }
               },
               icon: Icon(
@@ -113,18 +140,30 @@ class SendMessageTextField extends StatelessWidget {
                 if (messageController.text.trim() != '' &&
                     messageController.text.trim() != '') {
                   final user = FirebaseAuth.instance.currentUser;
-                  messages.add(
-                    {
-                      'message': messageController.text,
-                      'date': DateTime.now(),
-                      'id': user!.email,
-                      'name': user.displayName,
-                      'uid': user.uid,
-                      'type': 'text',
-                      'profilePhoto': user.photoURL,
-                    },
-                  );
-                  messageController.clear();
+                  bool result = await InternetConnectionChecker().hasConnection;
+                  if (result == true) {
+                    messages.add(
+                      {
+                        'message': messageController.text,
+                        'date': DateTime.now(),
+                        'id': user!.email,
+                        'name': user.displayName,
+                        'uid': user.uid,
+                        'type': 'text',
+                        'profilePhoto': user.photoURL,
+                      },
+                    );
+                    messageController.clear();
+                  } else {
+                    if (!mounted) return;
+                    showTopSnackBar(
+                      context,
+                      CustomSnackBar.error(
+                        message:
+                            "Oh! There's no internet connection. Please check your internet and try again",
+                      ),
+                    );
+                  }
                   scroolController.animateTo(0,
                       duration: Duration(milliseconds: 500),
                       curve: Curves.easeIn);
--- lib/component/send_voice_message.dart
+++ lib/component/send_voice_message.dart
@@ -3,9 +3,12 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:social_media_recorder/audio_encoder_type.dart';
 import 'package:social_media_recorder/screen/social_media_recorder.dart';
+import 'package:top_snackbar_flutter/custom_snack_bar.dart';
+import 'package:top_snackbar_flutter/top_snack_bar.dart';
 import '../component/consts.dart';
 
 class SendVoiceMessage extends StatelessWidget {
@@ -17,42 +20,46 @@ class SendVoiceMessage extends StatelessWidget {
   final CollectionReference<Map<String, dynamic>> messages;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, [bool mounted = true]) {
     return FutureBuilder<Directory?>(
         future: getTemporaryDirectory(),
-        builder: (context,
-            AsyncSnapshot<Directory?> snapshot) {
-          if (snapshot.connectionState ==
-              ConnectionState.done) {
+        builder: (context, AsyncSnapshot<Directory?> snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
             return SocialMediaRecorder(
-              storeSoundRecoringPath:
-                  '${snapshot.data?.path}',
+              storeSoundRecoringPath: '${snapshot.data?.path}',
               sendRequestFunction: (soundFile) async {
-                final file = File(soundFile.path);
-                final user =
-                    FirebaseAuth.instance.currentUser;
-                final path =
-                    'files/sounds/${user!.email}/${DateTime.now().toLocal().day}-${DateTime.now().toLocal().hour}-${DateTime.now().toLocal().minute}';
-                final ref = FirebaseStorage.instance
-                    .ref()
-                    .child(path);
-                UploadTask? uploadTask;
-                uploadTask = ref.putFile(file);
-                final snapshot = await uploadTask
-                    .whenComplete(() {});
-                final urlDownload =
-                    await snapshot.ref.getDownloadURL();
-                messages.add(
-                  {
-                    'message': urlDownload,
-                    'date': DateTime.now(),
-                    'id': user.email,
-                    'name': user.displayName,
-                    'uid': user.uid,
-                    'type': 'voice',
-                    'profilePhoto': user.photoURL,
-                  },
-                );
+                bool result = await InternetConnectionChecker().hasConnection;
+                if (result == true) {
+                  final file = File(soundFile.path);
+                  final user = FirebaseAuth.instance.currentUser;
+                  final path =
+                      'files/sounds/${user!.email}/${DateTime.now().toLocal().day}-${DateTime.now().toLocal().hour}-${DateTime.now().toLocal().minute}';
+                  final ref = FirebaseStorage.instance.ref().child(path);
+                  UploadTask? uploadTask;
+                  uploadTask = ref.putFile(file);
+                  final snapshot = await uploadTask.whenComplete(() {});
+                  final urlDownload = await snapshot.ref.getDownloadURL();
+                  messages.add(
+                    {
+                      'message': urlDownload,
+                      'date': DateTime.now(),
+                      'id': user.email,
+                      'name': user.displayName,
+                      'uid': user.uid,
+                      'type': 'voice',
+                      'profilePhoto': user.photoURL,
+                    },
+                  );
+                } else {
+                  if (!mounted) return;
+                  showTopSnackBar(
+                    context,
+                    CustomSnackBar.error(
+                      message:
+                          "Oh! There's no internet connection. Please check your internet and try again",
+                    ),
+                  );
+                }
               },
               encode: AudioEncoderType.AMR_NB,
               recordIcon: Padding(
@@ -69,8 +76,7 @@ class SendVoiceMessage extends StatelessWidget {
               backGroundColor: kGreyColor,
               recordIconBackGroundColor: Colors.red,
               counterBackGroundColor: kGreyColor,
-              recordIconWhenLockBackGroundColor:
-                  kGreyColor,
+              recordIconWhenLockBackGroundColor: kGreyColor,
               cancelTextBackGroundColor: kGreyColor,
               lockButton: Icon(Icons.lock),
             );
